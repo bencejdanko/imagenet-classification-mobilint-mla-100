55f371de82a5c7ed4931cb2f480886dce0e06df5
Bence Danko <bencejdanko@gmail.com>
2026-03-01 02:53:52 -0800

    correct config locations

--- 2026-03-01/alexnet-2/config.py
+++ 2026-03-01/alexnet-2/config.py
@@ -1,7 +1,7 @@
 class Config:
-    IMAGE_ROOT = 'imagenet_train20a'
+    IMAGE_ROOT = 'imagenet_train20a/imagenet_train20a'
     TRAIN_LIST = 'imagenet_train20.txt'
-    VAL_IMAGE_ROOT = 'imagenet_val20'
+    VAL_IMAGE_ROOT = 'imagenet_val20/imagenet_val20'
     VAL_LIST = 'imagenet_val20.txt'
     BATCH_SIZE = 512
     NUM_CLASSES = 20
